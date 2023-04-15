f9664dbe6e18e464bab05d8cbe9af09753f26a0b
sweder <swestrong9@gmail.com>
2023-04-15 14:17:08 +0200

    arduino tests

---
+++ sketch_mar23a/sketch_mar23a.ino
@@ -0,0 +1,37 @@
+// constants won't change. They're used here to set pin numbers:
+const int BUTTON_PIN = 7;       // the number of the pushbutton pin
+int x = 40;
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the pushbutton pin as an pull-up input
+  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  
+}
+
+void loop() {
+  // read the state of the switch/button:
+  int buttonState = digitalRead(BUTTON_PIN);
+
+  // print out the button's state 
+  Serial.println(x);
+  
+  if(buttonState == 0){
+      tone(8,x,10);
+      if(x<364){
+        x = x+1;
+        
+        }
+      else{
+        x = x-364;
+        
+        }
+      
+          
+         
+    }
+  
+  
+}
---
+++ test/test.ino
@@ -0,0 +1,14 @@
+int sum_func (int x, int y) {
+   int z = 0;
+   z = x+y ;
+   return z; // return the value
+}
+
+void setup () {
+//   Statements // group of statements
+}
+
+void loop () {
+   int result = 0 ;
+   int result = Sum_func (5,6) ; // function call
+}
